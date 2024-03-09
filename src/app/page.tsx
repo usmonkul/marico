575a23cf02b43ab73033d3a93b1bc09e0ff750b1
Usmon Khakim <usmonhakimov1999@gmail.com>
2024-03-09 16:42:09 +0900

    add hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import Header from "@/components/header/header";
 import Headings from "@/components/headings/headings";
+import Hero from "@/components/hero/hero";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col ">
-      <Header />
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <Hero />
+      </div>
     </main>
   );
 }
---
+++ src/components/hero/hero.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Button } from '../ui/button'
+
+const Hero = () => {
+    return (
+        <section className='h-full py-20 container flex-grow flex flex-col items-center justify-center md:gap-20 lg:gap-32'>
+            <h1 className="text-5xl md:text-6xl lg:text-8xl font-bold text-center">
+                Own your audience. <br />
+                <p className="bg-gradient-to-r from-[#FE6783] to-[#FFBF84] inline-block text-transparent bg-clip-text">So you dont&apos;t lose them.</p>
+            </h1>
+            <div className='flex flex-col items-center'>
+                <p className="text-2xl md:text-3xl lg:text-4xl font-semibold text-center mb-12">
+                    Turn your audience into email and <br />
+                    text message subscribers.
+                </p>
+                <div className="flex items-center gap-10 mb-5">
+                    <Button size={'lg'}>Get Started Now</Button>
+                    <Button size={'lg'} variant={'outline'}>View A Demo</Button>
+                </div>
+                <div className="flex items-center text-center justify-center font-bold">
+                    <div className="h-4 w-4 bg-green-300 rounded-full flex items-center justify-center mr-2">
+                        <div className="h-3 w-3 bg-green-600 rounded-full"></div>
+                    </div>
+                    1000+ <span className="ml-2 text-text-content font-normal">creators have already started</span>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Hero
